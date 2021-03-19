d615e5a69a14a35de3e1d96cf7e5a9c832eb7646
Claudiu98n <claudiuneagu9988@gmail.com>
2021-03-19 11:05:12 +0200

    Added table to pacients list

--- frontend/src/DoctorPages/PacientsList/PacientsList.jsx
+++ frontend/src/DoctorPages/PacientsList/PacientsList.jsx
@@ -1,13 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+// react-bootstrap
+import { Table, Button } from "react-bootstrap";
 
 class PacientsList extends Component {
-    render() {
-        return (
-            <div className="d-flex justify-content-center align-items-center">
-                ok
-            </div>
-        )
+    constructor() {
+        super();
+        this.state=[
+            {
+                id: 1,
+                name: 'Duffy',
+                surname: 'Duck'
+            }
+        ]
     }
+
+    render() {
+    return (
+      <div className="d-flex justify-content-center align-items-center">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Număr</th>
+              <th>Nume</th>
+              <th>Prenume</th>
+              <th>Accesează</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Larry</td>
+              <td>Larry</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
 
 export default PacientsList;
