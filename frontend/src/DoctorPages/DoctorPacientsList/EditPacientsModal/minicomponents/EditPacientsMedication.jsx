7bc7aff7c30255b3022bc4a91ec1ac799ae57d04
Claudiu98n <claudiuneagu9988@gmail.com>
2021-03-30 18:44:26 +0300

    Responsive on PacientsList + added form on modal

--- frontend/src/DoctorPages/DoctorPacientsList/EditPacientsModal/minicomponents/EditPacientsMedication.jsx
+++ frontend/src/DoctorPages/DoctorPacientsList/EditPacientsModal/minicomponents/EditPacientsMedication.jsx
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
-import { FormControl } from "react-bootstrap";
+import { FormControl, Form } from "react-bootstrap";
 
 class EditPacientsMedication extends Component {
   constructor() {
     super();
     this.state = {
-      hour: "",
+      medicament: null,
+      hour: null,
+      startDate: null,
+      endDate: null
     };
   }
-
-  handleHours = (event) => {
-    console.log(event);
-    this.setState({
-      hour: event,
-    });
-  };
 
   render() {
     console.log(this.props);
@@ -23,30 +19,32 @@ class EditPacientsMedication extends Component {
         <p className="h5">
           Adaugă medicație pentru {this.props.activePacient.username}
         </p>
+        <Form.Label htmlFor="medicament">Nume medicament</Form.Label>
         <FormControl
           type="text"
-          name=""
-          placeholder="Nume medicament"
+          name="medicament"
+          placeholder="Nurofen"
           onChange={this.handleChange}
         />
+        <Form.Label htmlFor="hour" className="mt-3">
+          Ora administrării
+        </Form.Label>
+        <FormControl type="time" name="hour" onChange={this.handleChange} />
+        <Form.Label htmlFor="startDate" className="mt-3">
+          Data începerii
+        </Form.Label>
         <FormControl
-          type="text"
-          name=""
-          className="mt-3"
-          placeholder="Ora administrării"
-          onChange={this.handleChange}
-        />
-        <FormControl
-          type="text"
-          name=""
-          className="mt-3"
+          type="date"
+          name="startDate"
           placeholder="Data începerii"
           onChange={this.handleChange}
         />
+        <Form.Label htmlFor="endDate" className="mt-3">
+          Data finalizării
+        </Form.Label>
         <FormControl
-          type="text"
-          name=""
-          className="mt-3"
+          type="date"
+          name="endDate"
           placeholder="Data finalizării"
           onChange={this.handleChange}
         />
